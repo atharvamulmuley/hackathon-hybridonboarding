f9e2cba2acae8ab5d64c11fecef1339d63bfed34
umeshbhat <umeshbhat@microsoft.com>
2022-09-23 13:55:39 +0530

    added logs

--- ParentCluster/app.js
+++ ParentCluster/app.js
@@ -1,23 +1,25 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
-app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
-const serverIps=[]
+const serverIps = []
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 app.put('/register', function (req, res) {
-    const {serverIp}= req.body
-    if(!serverIp) return res.send("server ip is missing")
+  const { serverIp } = req.body
 
-    serverIps.push(serverIp)
-    res.send('added')
-  })
-  app.get('/getserviceips', function (req, res) {
-    res.send(JSON.stringify(serverIps))
-  })
-app.listen(3000,() => {
-    console.log("server is up and running on port 3000",)
+  if (!serverIp) { console.log('serverIp missing from the req body'); return res.send("server ip is missing;") }
+  console.log('added new ip address', serverIp)
+  serverIps.push(serverIp)
+  res.send('added')
+})
+app.get('/getserviceips', function (req, res) {
+  console.log('sending all the ips')
+  res.send(JSON.stringify(serverIps))
+})
+app.listen(3000, () => {
+  console.log("server is up and running on port 3000",)
 })
